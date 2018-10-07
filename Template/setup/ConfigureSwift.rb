fb0f6e73a3dcfc5d3f6c345ec11a729f37cbf3e4
Tal Zion <talezion@gmail.com>
2018-10-07 15:29:43 +0200

    SFW-108 adding optional pods

--- Template/setup/ConfigureSwift.rb
+++ Template/setup/ConfigureSwift.rb
@@ -33,6 +33,8 @@ module Stanwood
       configurator.add_pod_to_podfile "'StanwoodDebugger', :configurations => ['Debug']"
       configurator.add_pod_to_podfile "'StanwoodDialog'"
       configurator.add_pod_to_podfile "'Firebase'"
+      configurator.add_pod_to_podfile "'ReachabilitySwift'"
+      configurator.add_pod_to_podfile "'MBProgressHUD'"
 
       add_moya = configurator.ask_with_answers("Would you like to add Moya", ["Yes", "No"]).to_sym
       case add_moya
@@ -40,12 +42,32 @@ module Stanwood
           configurator.add_pod_to_podfile "'Moya'"
       end
 
-      add_image_cache = configurator.ask_with_answers("Would you like to add SDWebImage || Kingficher", ["SDWebImage", "Kingficher"]).to_sym
+
+
+      add_rm_store = configurator.ask_with_answers("Would you like to add RMStore to handle IAP", ["Yes", "No"]).to_sym
+      case add_rm_store
+        when :yes
+          configurator.add_pod_to_podfile "'RMStore'"
+      end
+
+      add_keyboard_manager = configurator.ask_with_answers("IQKeyboardManagerSwift allows you to prevent issues of the keyboard sliding up and cover UITextField/UITextView without needing you to enter any code and no additional setup required.\n\nWould you like to add IQKeyboardManagerSwift", ["Yes", "No"]).to_sym
+      case add_keyboard_manager
+        when :yes
+          configurator.add_pod_to_podfile "'IQKeyboardManagerSwift'"
+      end
+
+      add_font_awesome = configurator.ask_with_answers("Would you like to add FontAwesome.swift", ["Yes", "No"]).to_sym
+      case add_font_awesome
+        when :yes
+          configurator.add_pod_to_podfile "'FontAwesome.swift'"
+      end
+
+      add_image_cache = configurator.ask_with_answers("Would you like to add SDWebImage || Kingficher", ["SDWebImage", "Kingfisher"]).to_sym
       case add_image_cache
       when :SDWebImage
-        configurator.add_pod_to_podfile "'Firebase'"
+        configurator.add_pod_to_podfile "'SDWebImage'"
       when :Kingficher
-        configurator.add_pod_to_podfile "'Kingficher'"
+        configurator.add_pod_to_podfile "'Kingfisher'"
       end
     end
   end
