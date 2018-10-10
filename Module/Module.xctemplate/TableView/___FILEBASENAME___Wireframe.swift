ea33673febdcb8d84aa1a728452629b2deaf5994
Tal Zion <talezion@gmail.com>
2018-10-10 14:58:14 +0300

    SFW-108 updating module template

--- Module/Module.xctemplate/TableView/___FILEBASENAME___Wireframe.swift
+++ Module/Module.xctemplate/TableView/___FILEBASENAME___Wireframe.swift
@@ -1,14 +1,14 @@
 //___FILEHEADER___
 
 class ___VARIABLE_productName___Wireframe {
-    
+
     static func makeViewController() -> ___VARIABLE_productName___ViewController {
-    	return UIStoryboard().instantiate(viewController: ___VARIABLE_productName___ViewController.self)
+      return UIStoryboard(name: ___VARIABLE_productName___ViewController.self.identifier, bundle: nil).instantiate(viewController: ___VARIABLE_productName___ViewController.self)
     }
-    
+
     static func prepare(_ viewController: ___VARIABLE_productName___ViewController, with  actions: ___VARIABLE_productName___Actionable, and parameters: ___VARIABLE_productName___Parameterable) {
     	let presenter =  ___VARIABLE_productName___Presenter(actionable: actions, parameterable: parameters, viewable: viewController)
         viewController.presenter = presenter
     }
-    
+
 }
