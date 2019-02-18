c735edad2c317eaad86f900c64569f1f131086df
Tal Zion <talezion@gmail.com>
2019-02-18 11:57:13 +0200

    Removing unused block

--- Script/install.sh
+++ Script/install.sh
@@ -12,8 +12,3 @@ sudo mv /tmp/xcode_template/Module/Module.xctemplate "/Applications/Xcode.app/Co
 
 echo "Finishing installation"
 rm -rf /tmp/xcode_template
-
-# curl "https://api.github.com/repos/stanwood/Xcode_Templates_iOS/contents/Module" \
-#      -H 'Authorization: token 7ced5cee4a16818c437b182eb034d73127b9078d' \
-#      -H 'Accept: application/vnd.github.v3.raw' \
-#      -H 'content-Type: application/json'
